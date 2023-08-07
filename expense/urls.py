81962992b9ba2c3eccc857515ec5e5f8797df339
joao cocco <jppirado2015@gmail.com>
2023-08-07 14:14:19 -0300

    adicionando grafico

--- expense/urls.py
+++ expense/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard, CreateExpense
+from .views import dashboard, CreateExpense, load_json_data
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     path('dashboard' , login_required(dashboard), name="expense.dashboard"),
+    path('data', load_json_data, name='pivot_data'),
     path('dashboard/create' , login_required(CreateExpense.as_view() ), name="expense.create"),
 ]
--- expense/views.py
+++ expense/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render
 from .forms import ExpenseForm
 from .models import Expense
 from django.views.generic import CreateView
+from django.core import serializers
+from django.http import JsonResponse
 
 def dashboard(request):
     
     qr = Expense.objects.all()
-    
 
     return render(  request , 'dashboard/dashboard.html' , { 'data' : qr} )
+
+def load_json_data(request):
+    dataset = Expense.objects.all()
+    data = serializers.serialize('json',dataset)
+    return JsonResponse(data, safe=False)
 
 class CreateExpense( CreateView ):
         model =  Expense
         form_class = ExpenseForm
         success_url = '/expense/dashboard'
+        
