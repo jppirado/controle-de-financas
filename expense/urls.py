9dc18befb0532dd81ae8d3a09942d0c028d5c214
joao cocco <jppirado2015@gmail.com>
2023-08-07 16:01:09 -0300

    criando listagem

--- expense/urls.py
+++ expense/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import dashboard, CreateExpense, load_json_data
+from .views import dashboard, CreateExpense, load_json_data, ListAllExpenses
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     path('dashboard' , login_required(dashboard), name="expense.dashboard"),
-    path('data', load_json_data, name='pivot_data'),
+    path('data', login_required(load_json_data), name='pivot_data'),
     path('dashboard/create' , login_required(CreateExpense.as_view() ), name="expense.create"),
+    path('dashboard/list' , login_required(ListAllExpenses.as_view()) , name='expense.list' ),
 ]
--- expense/views.py
+++ expense/views.py
@@ -1,15 +1,16 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from .forms import ExpenseForm
 from .models import Expense
 from django.views.generic import CreateView
 from django.core import serializers
 from django.http import JsonResponse
+from django.db.models import Sum
+from django.views.generic import ListView
 
 def dashboard(request):
-    
-    qr = Expense.objects.all()
-
-    return render(  request , 'dashboard/dashboard.html' , { 'data' : qr} )
+    soma = Expense.objects.filter(user=request.user).aggregate(total_sum=Sum('value'))
+    return render(  request , 'dashboard/dashboard.html' , {'total':soma } )
 
 def load_json_data(request):
     dataset = Expense.objects.filter(user=request.user)
@@ -30,10 +31,15 @@ class CreateExpense( CreateView ):
         return data 
         
     def form_valid(self, form):
-        print(self) 
-        print(form)
-        print(dir(form))
         if form.is_valid():
             form.instance.user = self.request.user       
             form.save()
         return super(CreateExpense, self).form_valid(form)
+    
+class ListAllExpenses(ListView):
+    model = Expense
+    
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context  = super().get_context_data(**kwargs)
+        context['object_list'] = Expense.objects.filter(user=self.request.user)
+        return context
